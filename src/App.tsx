37abdd118f380753584978129e9f66cc0aba770b
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-11-03 05:21:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Today</h1>
+      <h2>2 Nov • Saturday</h2>
+      <div>
+        <input type='checkbox' name='check1' />
+        <label for='check1'>Read book for 15 minutes</label>
+      </div>
+      <aside>
+        <div>Today</div>
+        <div>Upcoming</div>
+      </aside>
+    </>
+  )
+}
+
+export default App
