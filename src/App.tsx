fcf3a9d80fec211ff554146f2646063c0f45522d
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-11-07 15:43:39 +0800

    chores: adding eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Calendar from './Main/Calendar';
 import './index.css'
 
 function App() {
+<<<<<<< Updated upstream
   const [page, setPage] = useState('today');
 
   const handlePage = (data: string) => {
@@ -28,6 +29,24 @@ function App() {
       )}
     </>
   )
+=======
+    const unusedVar = 1;
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Today</h1>
+            <h2>2 Nov • Saturday</h2>
+            <div>
+                <input type="checkbox" name="check1" />
+                <label htmlFor="check1">Read book for 15 minutes</label>
+            </div>
+            <aside>
+                <div>Today</div>
+                <div>Upcoming</div>
+            </aside>
+        </>
+    )
+>>>>>>> Stashed changes
 }
 
 export default App
