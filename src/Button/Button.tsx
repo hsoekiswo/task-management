c922cf721bac231443086e8c4df8bb9b4f4d018c
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-11-13 10:40:53 +0800

    fix: screen, nav and button display

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,7 @@
 export default function NewTaskButton({ isActive, onShow }) {
   return (
-    <div className='new-task'>
-        <button className='btn-new-task' onClick={onShow}>+</button>
-    </div>
+      <div className="new-task-container">
+          <button className='btn-new-task' onClick={onShow}>+</button>
+      </div>
   )
 }
