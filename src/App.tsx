74e901a2aee7725677cac896a8b6b6e39503539d
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2024-11-13 11:40:14 +0800

    refactor: splitting Nav button into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 import Nav from './Nav/Nav'
 import Today from './Main/Today'
 import Calendar from './Main/Calendar';
 import CreateNew from './Checklist/CreateNew';
-import NewTaskButton from './Button/Button';
+import { NewTaskButton } from './Button/Button';
 import './index.css'
 
 function App() {
